7f821a47b70ab6ab468bd3371d42c7c6c45a732d
Lawrence <beutlichlawrenceiii@gmail.com>
2022-04-24 16:28:52 +0100

    added wget

--- setup_exporter.sh
+++ setup_exporter.sh
@@ -8,7 +8,7 @@ chmod +x /usr/local/bin/node_exporter
 useradd -m -s /bin/bash node_exporter
 mkdir /var/lib/node_exporter
 chown -R node_exporter:node_exporter /var/lib/node_exporter
-https://gist.githubusercontent.com/ipl31/1f6a00f40448c9e5d78d49c02a7f1caa/raw/b05e28a6ca1c89e815747e8f7e186a634518f9c1/node_exporter.service
+wget https://gist.githubusercontent.com/ipl31/1f6a00f40448c9e5d78d49c02a7f1caa/raw/b05e28a6ca1c89e815747e8f7e186a634518f9c1/node_exporter.service
 mv node_exporter.service /etc/systemd/system
 systemctl daemon-reload
 systemctl enable node_exporter.service
